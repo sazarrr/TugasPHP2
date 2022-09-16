dfc8d352bac3d898a86e0b5bb161df7c69524b10
sazarrr <97484837+sazarrr@users.noreply.github.com>
2022-09-16 10:00:25 +0700

    Add files via upload

---
+++ data.php
@@ -0,0 +1,46 @@
+<?php 
+
+    $user = [
+        [
+            "username" => "admin1",
+            "password" => "admin1",
+            "img" => "foto1",
+            "alamat" => "alamat admin1",
+            "tlp" => "085866188133", 
+            "role" => "admin"
+        ],
+        [
+            "username" => "admin2",
+            "password" => "admin2",
+            "img" => "foto2",
+            "alamat" => "alamat admin2",
+            "tlp" => "081371228166",
+            "role" => "admin"
+        ],
+        [
+            "username" => "siswa1",
+            "password" => "siswa1",
+            "img" => "foto3",
+            "alamat" => "alamat siswa1",
+            "tlp" => "085866188133",
+            "role" => "siswa"
+        ],
+        [
+            "username" => "siswa2",
+            "password" => "siswa2",
+            "img" => "foto4",
+            "alamat" => "alamat siswa2",
+            "tlp" => "081371228166",
+            "role" => "siswa"
+        ],
+        [
+            "username" => "siswa3",
+            "password" => "siswa3",
+            "img" => "foto5",
+            "alamat" => "alamat siswa3",
+            "tlp" => "085866188133",
+            "role" => "siswa"
+        ],
+    ];
+
+?>
